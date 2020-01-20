e1c7ab14c20b27b987eaf3a5cf971aaaace048e8
zeynepozturkmen <zynpoztrkmn@hotmail.com>
2020-01-20 13:29:52 +0300

    Adding products to the cart

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,40 +1,49 @@
-import React, { Component } from 'react';
-import {ListGroup,ListGroupItem} from 'reactstrap';
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-   //props anahtar kelimesi ile props oluşturuldu.Super kelimesiyle extends edildi.
-    //Eski sürümde bunu yazmaya zorunlu tutuyor galiba,şuanda bunu yazmadan direk,this.props diyerek erişebiliriz.
-  state={ categories:[
-            {categoryId:1,categoryName:"Beverages"},
-            {categoryId:2,categoryName:"Condinents"},
-            ],
-        };
+  //props anahtar kelimesi ile props oluşturuldu.Super kelimesiyle extends edildi.
+  //Eski sürümde bunu yazmaya zorunlu tutuyor galiba,şuanda bunu yazmadan direk,this.props diyerek erişebiliriz.
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condinents" }
+    ]
+  };
   //komponentlerin sayfaya yerleşmesini sağlıyor
-        componentDidMount(){
-            this.getCategories();
-        }
+  componentDidMount() {
+    this.getCategories();
+  }
 
-        getCategories=()=>{
-            fetch("http://localhost:3000/categories").then(response=>response.json()).then(data=>this.setState({categories:data}));;
-        }
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then(response => response.json())
+      .then(data => this.setState({ categories: data }));
+  };
 
-    render() {
-        //this kelimesi=CategoryList'e denk geliyor.
-  return (
+  render() {
+    //this kelimesi=CategoryList'e denk geliyor.
+    return (
       <div>
-    
-<h3>{this.props.info.title}</h3>
-<h3>{this.state.counter}</h3>
-    <ListGroup>
-     {
-         this.state.categories.map(category=>(
-         <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.id}>{category.categoryName}</ListGroupItem>          
-         ))
-     }
-    </ListGroup>
-    <h4>{this.props.currentCategory}</h4>
-    </div>
-  );
-
+        <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
+        <ListGroup>
+          {this.state.categories.map(category => (
+            <ListGroupItem
+              key={category.id}
+              active={
+                category.categoryName === this.props.currentCategory
+                  ? true
+                  : false
+              }
+              onClick={() => this.props.changeCategory(category)}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        <h4>{this.props.currentCategory}</h4>
+      </div>
+    );
+  }
 }
-    }
--- intro/src/Navi.js
+++ intro/src/Navi.js
@@ -1,12 +1,64 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
-export default class Navi extends Component {
-   
-    render() {
-        return (
-            <div>
-              <h2>Navi Component</h2>  
-            </div>
-        )
-    }
+
+
+
+const Navi = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+       {console.log(`propsda ne var ${JSON.stringify(props.cart)}`)}
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Nortwind App</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options - {props.cart.length}
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Navi;
